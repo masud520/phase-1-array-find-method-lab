7936f63cdd56cd011acaeac5260f0216a846588e
masud520 <masud.abdi@student.moringaschool.com>
2023-10-18 11:06:48 +0300

    completed the assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winYear = record.find(entry => entry.result === 'W');
+    if(winYear){
+        return winYear.year;
+    }else{
+        return undefined
+    }
+  }
+  
